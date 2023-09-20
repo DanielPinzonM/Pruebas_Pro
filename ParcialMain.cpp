4ec3493990864dbb9b034ad63604b2fc90ac4a14
Daniel Pinzón <daniele.pinzon@udea.edu.co>
2023-09-20 11:50:13 -0500

    Se añade la declaración y configuración de los puertos digitales Arduino para el control de filas

---
+++ ParcialMain.cpp
@@ -0,0 +1,13 @@
+#define SerF 8
+#define RclkF 9
+#define SrclkF 10
+
+void setup(){
+    pinMode(SerF, OUTPUT);
+    pinMode(RclkF, OUTPUT);
+    pinMode(SrclkF, OUTPUT);
+}
+
+void loop(){
+    //contenido
+}
