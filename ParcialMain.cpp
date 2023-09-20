6a1d9758bb98850c07f00f000c909563b983811d
Daniel Pinzón <daniele.pinzon@udea.edu.co>
2023-09-20 13:00:59 -0500

    Se incluyen las declaraciones y configuración de los pines para las columnas

--- ParcialMain.cpp
+++ ParcialMain.cpp
@@ -1,8 +1,20 @@
+//Puertos de Columnas
+#define SerC 5
+#define RclkC 6
+#define SrclkC 7
+
+//Puertos de Filas
 #define SerF 8
 #define RclkF 9
 #define SrclkF 10
 
 void setup(){
+    //Set up Columnas
+    pinMode(SerC, OUTPUT);
+    pinMode(RclkC, OUTPUT);
+    pinMode(SrclkC, OUTPUT);
+    
+    //Set up Filas
     pinMode(SerF, OUTPUT);
     pinMode(RclkF, OUTPUT);
     pinMode(SrclkF, OUTPUT);
