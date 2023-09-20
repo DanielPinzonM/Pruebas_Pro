0c7042affa7fe3f74c915b9c8dd2902ff665b1dc
Daniel Pinzón <daniele.pinzon@udea.edu.co>
2023-09-20 14:14:00 -0500

    Se implementa el menú principal, lectura por monitor serie y filtrado de opciones

--- ParcialMain.cpp
+++ ParcialMain.cpp
@@ -18,8 +18,38 @@ void setup(){
     pinMode(SerF, OUTPUT);
     pinMode(RclkF, OUTPUT);
     pinMode(SrclkF, OUTPUT);
+
+    Serial.begin(9600);
 }
 
 void loop(){
-    //contenido
+    char Opcion;
+
+    Serial.println("\n\n\n\n\n\n\n\n\n\n\n");
+    Serial.println("Seleccione una de las siguientes opciones: \n");
+    Serial.println("1. Verificar leds");
+    Serial.println("2. Ingresar imagen por teclado");
+    Serial.println("3. Ejecutar patrones predeterminados");
+
+    while(Serial.available() == 0){
+    
+    }
+    Opcion = Serial.read();
+
+    switch(Opcion){
+        case '1':
+            //ejecutar función correspondiente
+            break;
+        case '2':
+            //ejecutar función correspondiente;
+            break;
+        case '3':
+            //ejecutar función correspondiente;
+        default:
+            Serial.print("\n<");
+            Serial.print(Opcion);
+            Serial.print(">");
+            Serial.println(" no es una opcion valida");
+            delay(3000);
+    }
 }
